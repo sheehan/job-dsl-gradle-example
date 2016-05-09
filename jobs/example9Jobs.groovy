9d6260b44cefdd832ee3f854f107b24f15881ea7
Daniel Spilker <daniel.spilker@coremedia.com>
2016-05-09 17:35:05 +0200

    use Jenkins test harness for testing extensions and automatically generated DSL

---
+++ jobs/example9Jobs.groovy
@@ -0,0 +1,52 @@
+String basePath = 'example9'
+String repo = ':pserver:anoncvs@example.org:/cvsroot'
+
+folder(basePath) {
+    description 'This example shows hwp to use automatically generated DSL provided by other plugins.'
+}
+
+job("$basePath/grails-legacy-build") {
+    scm {
+        cvsscm {
+            repositories {
+                cvsRepository {
+                    cvsRoot repo
+                    passwordRequired false
+                    password null
+                    compressionLevel(-1)
+                    repositoryBrowser {}
+                    repositoryItems {
+                        cvsRepositoryItem {
+                            modules {
+                                cvsModule {
+                                    remoteName 'sheehan/grails-example'
+                                    localName '.'
+                                    projectsetFileName null
+                                }
+                            }
+                            location {
+                                headRepositoryLocation()
+                            }
+                        }
+                    }
+                }
+            }
+            canUseUpdate true
+            pruneEmptyDirectories true
+            legacy false
+            skipChangeLog false
+            disableCvsQuiet false
+            cleanOnFailedUpdate false
+            forceCleanCopy false
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        grails {
+            useWrapper true
+            targets(['test-app', 'war'])
+        }
+    }
+}
