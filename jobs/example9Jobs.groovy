078e62f370a9e0958a4b93627227e64d730e4fd7
Matt Sheehan <mr.sheehan@gmail.com>
2017-09-26 14:56:58 -0500

    typo

--- jobs/example9Jobs.groovy
+++ jobs/example9Jobs.groovy
@@ -2,7 +2,7 @@ String basePath = 'example9'
 String repo = ':pserver:anoncvs@example.org:/cvsroot'
 
 folder(basePath) {
-    description 'This example shows hwp to use automatically generated DSL provided by other plugins.'
+    description 'This example shows how to use the automatically generated DSL provided by other plugins.'
 }
 
 job("$basePath/grails-legacy-build") {
